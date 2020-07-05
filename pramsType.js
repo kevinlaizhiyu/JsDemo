29adf2632e5d78cce5d5a117e1c3637b1a20c877
kevinlaizhiyu <928022781@qq.com>
2020-07-05 10:20:53 +0800

    july/05

--- pramsType.js
+++ pramsType.js
@@ -15,7 +15,7 @@ objA.name = 'David';
 console.log('objA.name--->',objA.name);
 console.log('objB.name--->',objB.name);
 
-// 
+// ECMAScript中参数传递是引用类型
 
 function addTen(num){
     num += 10;
@@ -25,3 +25,23 @@ var count = 20;
 var result = addTen(count); 
 console.log(count);//20
 console.log(result);//30
+
+console.log('----参数传递是对象的时候，证明是值得传递而非地址的传递----')
+
+function setName(obj){
+    obj.name = 'David';
+    var obj = new Object();
+    obj.name = 'Kevin';
+}
+
+var person = new Object();
+setName(person);
+console.log(person.name)
+
+console.log('------instaceof对象的检测------')
+var person = new Object();
+var color = new Array();
+var pattern = new RegExp();
+console.log(person instanceof Object);//true 'person 是 Object对象吗？'
+console.log(color instanceof Array);//true 'color 是 Array对象吗？'
+console.log(pattern instanceof RegExp);//true 'pattern 是 RegExp 对象吗？'
